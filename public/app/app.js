e3a712cb5814fb4e02227049d9ac8eaba7071c0a
romeiroe <romeiroe@gmail.com>
2014-04-04 15:36:54 -0400

    change include src for new partials

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,6 @@ angular.module('myApp', ['ngResource', 'ngRoute']);
 
 angular.module('myApp').config(function($routeProvider, $locationProvider){
    $locationProvider.html5Mode(true);
-   $routeProvider.when('/', {templateUrl: '/partials/main', controller: 'mainCtrl'});
+   $routeProvider.when('/', {templateUrl: '/partials/main/main', controller: 'mainCtrl'});
 });
 
