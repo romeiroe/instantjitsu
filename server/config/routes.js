dd7bb5d2349522a05dcdab92d88708fe5f25b44d
romeiroe <romeiroe@gmail.com>
2014-04-05 01:42:56 -0400

    login routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,20 @@
+var passport = require('passport');
+
 module.exports = function(app){
+   
    app.get('/partials/*', function(req, res){
       res.render('../../public/app/' + req.params);
+   });
+
+   app.post('/login', function(req,res,next){
+      var auth = passport.authenticate('local', function(err, user){
+         if (err){
+            return next(err);
+         }
+         if(!user){
+            res.send({success: false});
+         }
+      })
    });
 
    app.get('*', function(req, res){
