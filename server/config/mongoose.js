0cd627f43169c92efeedc85ac425dd7f45005cf4
romeiroe <romeiroe@gmail.com>
2014-04-04 20:51:13 -0400

    db work, added user schema

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,21 @@ module.exports = function(config){
    db.once('open', function callback() {
      console.log('instantjitsu db opened');
    });
+
+   var userSchema = mongoose.Schema({
+      firstName: String,
+      lastName: String,
+      userName: String
+   });
+
+   var User = mongoose.model('User', userSchema);
+
+   // create default users if collection is empty
+   User.find({}).exec(function(err,collection){
+      if (collection.length === 0) {
+         User.create({firstName:'Eduardo', lastName:'Romeiro', userName:'eddie'});
+         User.create({firstName:'Carlos', lastName:'Miranda', userName:'carlos'});
+         User.create({firstName:'Norberto', lastName:'Caceres', userName:'norberto'});
+      }
+   });
 }
