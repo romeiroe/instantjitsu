2249f8d46fa77a1a6790ed711a2cac0e1bd33646
root <root@cruzeiro.(none)>
2014-04-07 16:18:27 -0400

    hide login bar if logged in

--- public/app/account/loginCtrl.js
+++ public/app/account/loginCtrl.js
@@ -1,4 +1,5 @@
 angular.module('myApp').controller('loginCtrl', function($scope, $http, ijIdentity, ijNotifier){
+   $scope.identity = ijIdentity;
    $scope.signin = function(username,password) {
       $http.post('/login', {userName:username, password:password}).then(function(response){
          if(response.data.success){
