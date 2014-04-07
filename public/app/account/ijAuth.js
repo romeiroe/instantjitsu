69dfa8b2f442b66edab19855f99aadcb1d7afff4
root <root@cruzeiro.(none)>
2014-04-07 16:34:13 -0400

    created auth service

---
+++ public/app/account/ijAuth.js
@@ -0,0 +1,17 @@
+angular.module('myApp').factory('ijAuth', function($http, ijIdentity, $q){
+   return {
+      authenticateUser: function(username,password) {
+         var dfd = $q.defer();
+         $http.post('/login', {userName:username, password:password}).then(function(response){
+            if(response.data.success){
+               ijIdentity.currentUser = response.data.user;
+               dfd.resolve(true);
+            }
+            else{
+               dfd.resolve(false);
+            }
+         });
+         return dfd.promise;
+      }
+   }
+});
