ad0ff82961b4a60962c3fa4fdb38a7a221d8768c
root <root@cruzeiro.(none)>
2014-04-07 16:05:33 -0400

    fixed bug in toastr

--- public/app/common/ijNotifier.js
+++ public/app/common/ijNotifier.js
@@ -3,7 +3,7 @@ angular.module('myApp').value('ijToastr', toastr);
 angular.module('myApp').factory('ijNotifier', function(ijToastr){
    return {
       notify: function(msg){
-         ivToastr.success(msg);
+         ijToastr.success(msg);
          console.log(msg);
       }
    }
