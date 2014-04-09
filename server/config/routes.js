da6b2dd660aa63bc90838d4e15388d35ec60063b
root <root@cruzeiro.(none)>
2014-04-09 19:55:10 -0400

    require admin role for api access

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,7 +4,7 @@ var auth = require('./auth'),
 
 module.exports = function(app){
    
-   app.get('/api/users', auth.requiresApiLogin, function(req, res) {
+   app.get('/api/users', auth.requiresRole('admin'), function(req, res) {
       User.find({}).exec(function(err, collection){
          res.send(collection);
       });
