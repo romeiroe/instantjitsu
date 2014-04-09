14f92ec257b333a3703c29850e8849546d8b8e17
root <root@cruzeiro.(none)>
2014-04-09 19:31:35 -0400

    added user service

--- public/app/account/ijIdentity.js
+++ public/app/account/ijIdentity.js
@@ -10,4 +10,4 @@ angular.module('myApp').factory('ijIdentity', function($window, ijUser){
          return !!this.currentUser;
       }
    }
-})
+});
--- public/app/account/ijUser.js
+++ public/app/account/ijUser.js
@@ -6,4 +6,4 @@ angular.module('myApp').factory('ijUser', function($resource) {
    }
 
    return UserResource;
-})
+});
