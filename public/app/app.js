b7364c74faf7fe3cc92cff24c39b1fff4aa463c8
root <root@cruzeiro.(none)>
2014-04-18 12:03:26 -0400

    adding admin page on client

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,8 @@ angular.module('myApp', ['ngResource', 'ngRoute']);
 
 angular.module('myApp').config(function($routeProvider, $locationProvider){
    $locationProvider.html5Mode(true);
-   $routeProvider.when('/', {templateUrl: '/partials/main/main', controller: 'mainCtrl'});
+   $routeProvider
+   .when('/', {templateUrl: '/partials/main/main', controller: 'mainCtrl'});
+   .when('/admin/users', {templateUrl: '/partials/admin/user-list', controller: 'userListCtrl'});
 });
 
