5ecc6906493edede9824201dc51852f7baa9efe1
root <root@cruzeiro.(none)>
2014-04-18 18:57:06 -0400

    refactored server model

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    encrypt = require('../utilities/encryption');
+    userModel = require('../models/User');
 
 module.exports = function(config){
    mongoose.connect(config.db);
@@ -9,37 +9,7 @@ module.exports = function(config){
      console.log('instantjitsu db opened');
    });
 
-   var userSchema = mongoose.Schema({
-      firstName: String,
-      lastName: String,
-      username: String,
-      salt: String,
-      hashed_pwd: String,
-      roles: [String]
-   });
-   userSchema.methods = {
-      authenticate : function(passwordToMatch){
-         return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-      }
-   }
-
-   var User = mongoose.model('User', userSchema);
-
-   // create default users if collection is empty
-   User.find({}).exec(function(err,collection){
-      if (collection.length === 0) {
-         var salt, hash;
-         salt = encrypt.createSalt();
-         hash = encrypt.hashPwd(salt, 'eddie');
-         User.create({firstName:'Eduardo', lastName:'Romeiro', username:'eddie', salt: salt, hashed_pwd: hash, roles: ['admin']});
-         salt = encrypt.createSalt();
-         hash = encrypt.hashPwd(salt, 'carlos');
-         User.create({firstName:'Carlos', lastName:'Miranda', username:'carlos', salt: salt, hashed_pwd: hash, roles: []});
-         salt = encrypt.createSalt();
-         hash = encrypt.hashPwd(salt, 'noberto');
-         User.create({firstName:'Norberto', lastName:'Caceres', username:'norberto', salt: salt, hashed_pwd: hash});
-      }
-   });
+   userModel.createDefaultUsers();
 }
 
 
---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose'),
+    encrypt = require('../utilities/encryption');
+
+var userSchema = mongoose.Schema({
+   firstName: String,
+   lastName: String,
+   username: String,
+   salt: String,
+   hashed_pwd: String,
+   roles: [String]
+});
+userSchema.methods = {
+   authenticate : function(passwordToMatch){
+      return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+   }
+}
+
+var User = mongoose.model('User', userSchema);
+
+// create default users if collection is empty
+function createDefaultUsers() {
+   User.find({}).exec(function(err,collection){
+      if (collection.length === 0) {
+         var salt, hash;
+         salt = encrypt.createSalt();
+         hash = encrypt.hashPwd(salt, 'eddie');
+         User.create({firstName:'Eduardo', lastName:'Romeiro', username:'eddie', salt: salt, hashed_pwd: hash, roles: ['admin']});
+         salt = encrypt.createSalt();
+         hash = encrypt.hashPwd(salt, 'carlos');
+         User.create({firstName:'Carlos', lastName:'Miranda', username:'carlos', salt: salt, hashed_pwd: hash, roles: []});
+         salt = encrypt.createSalt();
+         hash = encrypt.hashPwd(salt, 'noberto');
+         User.create({firstName:'Norberto', lastName:'Caceres', username:'norberto', salt: salt, hashed_pwd: hash});
+      }
+   });
+};
+
+module.exports.createDefaultUsers = createDefaultUsers;
