064f5e6101aa639a734c48d84a879d0a2d298c99
root <root@cruzeiro.(none)>
2014-04-18 20:28:43 -0400

    controler typo

--- public/app/account/ijProfileCtrl.js
+++ public/app/account/ijProfileCtrl.js
@@ -1,4 +1,4 @@
-angular.module('myApp').controller('ijProfileCrtl', function($scope, ijAuth, ijIdentity, ijNotifier){
+angular.module('myApp').controller('ijProfileCtrl', function($scope, ijAuth, ijIdentity, ijNotifier){
    $scope.email = ijIdentity.currentUser.username;
    $scope.fname = ijIdentity.currentUser.firstName;
    $scope.lname = ijIdentity.currentUser.lastName;
