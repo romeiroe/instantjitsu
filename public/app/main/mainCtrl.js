bb0b3647e901a3b626078c3172e627a9eb1c5371
root <root@cruzeiro.(none)>
2014-04-19 13:34:07 -0400

    added filtering and searching

--- public/app/main/mainCtrl.js
+++ public/app/main/mainCtrl.js
@@ -1,14 +1,3 @@
-angular.module('myApp').controller('mainCtrl', function($scope) {
-   $scope.videos = [
-      {name: 'Guard Passing', featured: true, published: new Date('1/1/2014')},
-      {name: 'Spider Guard', featured: true, published: new Date('1/1/2014')},
-      {name: 'Pressue Passing', featured: false, published: new Date('1/1/2014')},
-      {name: 'Kimura Locks', featured: false, published: new Date('1/1/2014')},
-      {name: 'Arm Bars', featured: true, published: new Date('1/1/2014')},
-      {name: 'Arm Bar Counters', featured: false, published: new Date('1/1/2014')},
-      {name: 'Take Downs', featured: true, published: new Date('1/1/2014')},
-      {name: 'Competition Strategy', featured: false, published: new Date('1/1/2014')},
-      {name: 'Turtle Attacks', featured: true, published: new Date('1/1/2014')},
-      {name: 'Open Guard Sweeps', featured: true, published: new Date('1/1/2014')}
-   ]
+angular.module('myApp').controller('mainCtrl', function($scope, ijVideo) {
+   $scope.videos = ijVideo.query();
 });
--- public/app/videos/ijVideoCtrl.js
+++ public/app/videos/ijVideoCtrl.js
@@ -1,3 +1,7 @@
 angular.module('myApp').controller('ijVideoCtrl', function($scope, ijVideo){
    $scope.videos = ijVideo.query();
+
+   $scope.sortOptions = [{value: "title", text:"Sort by Title"},
+      {value: "published", text:"Sorth by published date"}];
+   $scope.sortOrder = $scope.sortOptions[0].value;
 });
